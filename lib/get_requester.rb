c00f2c8144a520c674ccbc5ce9c2011fdf5fd46d
Craig Ford <craig_ford@icloud.com>
2022-08-25 14:38:08 -0400

    commit tree d52285b30c060a838ec3661b8d373a488c6fc20e

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,33 @@
 # Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+require 'pry'
+class GetRequester
+
+    attr_reader :url
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body  
+    end
+
+    def parse_json
+        # we use the JSON library to parse the API response into nicely formatted JSON
+          req_arr = []
+          requesters = JSON.parse(self.get_response_body)
+          requesters.collect do |request|
+            req_arr << request
+          end
+          req_arr
+    end
+
+end
+
+requesters = GetRequester.new("https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json")
+puts requesters.parse_json
